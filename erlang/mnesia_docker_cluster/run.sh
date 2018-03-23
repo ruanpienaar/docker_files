948a2a53fac7327025309d4d2a969e0d2a39d8bb
ruanpienaar <ruan800@gmail.com>
2018-03-23 14:00:30 +0000

    ..

--- erlang/mnesia_docker_cluster/run.sh
+++ erlang/mnesia_docker_cluster/run.sh
@@ -3,4 +3,4 @@
 # $2 = Nodename            : node@host
 # $3 = Cookie              : cookie
 # $4 = Ip                  : 127.0.0.1
-docker run -name "mnesia_docker_cluster_example_$2" --net $1  -td -e NODENAME=$2 -e COOKIE=$3 --ip $4 mnesia_docker_cluster_example:latest
+docker run --name "mnesia_docker_cluster_example_$2" --net $1  -td -e NODENAME=$2 -e COOKIE=$3 --ip $4 mnesia_docker_cluster_example:latest
