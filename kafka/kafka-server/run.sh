d561dc297194012d456905695c5bb613fe7a1544
ruanpienaar <ruan800@gmail.com>
2019-03-06 21:16:17 +0000

    Create Docker images with kafka broker port 9092, but expose differently

--- kafka/kafka-server/run.sh
+++ kafka/kafka-server/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
-if [ -z ${BROKER_PORT+x} ]; then echo "BROKER_PORT is unset"; exit; fi
+#if [ -z ${BROKER_PORT+x} ]; then echo "BROKER_PORT is unset"; exit; fi
 if [ -z ${ZOOKEEPER_HOSTNAME+x} ]; then echo "ZOOKEEPER_HOSTNAME is unset"; exit; fi
 if [ -z ${BROKER_ID+x} ]; then echo "BROKER_ID is unset"; exit; fi
 cd kafka_2.11-2.1.0 && \
 HN=`hostname`
-echo "listeners=PLAINTEXT://$HN:$BROKER_PORT" >> config/server.properties
+echo "listeners=PLAINTEXT://$HN:9092" >> config/server.properties
 echo "zookeeper.connect=$ZOOKEEPER_HOSTNAME:2181" >> config/server.properties
 echo "broker.id=$BROKER_ID" >> config/server.properties
 echo "Starting kafka server with properties:"
